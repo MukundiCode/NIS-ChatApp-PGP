b5cf5f4d82c810fefb5c295d80a9b48f46273988
Suvanth Ramruthen <rmrsuv002@cs.uct.ac.za>
2022-06-05 13:31:45 +0200

    pgp system and encryption

--- bcpg-jdk18on-171/src/org/bouncycastle/openpgp/operator/SessionKeyDataDecryptorFactory.java
+++
@@ -1,9 +0,0 @@
-package org.bouncycastle.openpgp.operator;
-
-import org.bouncycastle.openpgp.PGPSessionKey;
-
-public interface SessionKeyDataDecryptorFactory
-    extends PGPDataDecryptorFactory
-{
-    public abstract PGPSessionKey getSessionKey();
-}
