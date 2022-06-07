37017bee7ad750f4333d4f98c6d4c7498cfc6ed4
Mujaahid2 <105645873+Mujaahid2@users.noreply.github.com>
2022-06-07 11:36:28 +0200

    Added Test class for Certificate methods

--- ReadMe.txt
+++ ReadMe.txt
@@ -15,3 +15,7 @@ java -cp .;1.jar;2.jar;3.jar;4.jar;5.jar;6.jar Server
 
 To run the Client on Windows use command:
 java -cp .;1.jar;2.jar;3.jar;4.jar;5.jar;6.jar Client
+
+To test the Certifcate methods compile and run the TestCertificate class:
+javac -cp .;1.jar;2.jar;3.jar;4.jar;5.jar;6.jar *.java
+java -cp .;1.jar;2.jar;3.jar;4.jar;5.jar;6.jar TestCertificate
--- src/GroupChat-TCP/Certificates.java
+++ src/GroupChat-TCP/Certificates.java
@@ -77,8 +77,6 @@ public class Certificates {
      * @param caPublicKey CA Public Key
      * @return boolean value for whether certifcate is valid or not
      */
-
-    // validates client certificate with CA to determine if public key belongs to client
     public static boolean validateCertificate(X509Certificate certificate, PublicKey caPublicKey){
         
         try {
@@ -90,24 +88,63 @@ public class Certificates {
         }  
     }
 
+    /**
+     * Generates a client certificate and signs it with a fake CA private key
+     * @param certificate certifcate of client
+     * @param fakeCAKey Fake CA's Private Key
+     * @param clientName client username
+     * @return certficate Client certificate
+     */
+    public static X509Certificate generateFakeCertificate(PublicKey clientKey, PrivateKey fakeCAKey, String clientName) throws CertificateException, OperatorCreationException, KeyStoreException, NoSuchAlgorithmException, FileNotFoundException, IOException {
+        Calendar certExpireDate = Calendar.getInstance();
+        certExpireDate.add(Calendar.YEAR, 1);
+
+        X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(
+                new X500Name("CN="+clientName),
+                BigInteger.ONE,
+                new Date(),
+                certExpireDate.getTime(),
+                new X500Name("CN="+clientName),
+                SubjectPublicKeyInfo.getInstance(clientKey.getEncoded())
+        );
+        //Create and sign certificate
+        X509CertificateHolder certHolder = certificateBuilder.build(new JcaContentSignerBuilder("SHA1withRSA").build(fakeCAKey));
+        X509Certificate certificate = new JcaX509CertificateConverter().getCertificate(certHolder);
+        //storeCertificate(certificate, clientName);
+
+        return certificate;
+    }
+
 
     /*
      * test the methods of the class
      */
-    public void test() throws CertificateException, OperatorCreationException, NoSuchAlgorithmException, KeyStoreException, FileNotFoundException, IOException {
+    public static void test() throws CertificateException, OperatorCreationException, NoSuchAlgorithmException, KeyStoreException, FileNotFoundException, IOException {
         // create and generate Server/CA key pair and root certificate
         RSA keyPair = new RSA();
         X509Certificate caCert = generateCACertificate(keyPair);
         System.out.println("CA certificate: "+caCert.toString());
-        System.out.println("CA Public Key: "+caCert.getPublicKey());
+        System.out.println();
         // create and client1 key pair and signed certificate
         RSA keyPairClient1 = new RSA();
         X509Certificate c1Cert = generateClientCertificate(keyPairClient1.getPublic(), "client1");
         System.out.println("Client 1 certificate: "+c1Cert.toString());
+        System.out.println();
         // create and client2 key pair and signed certificate
         RSA keyPairClient2 = new RSA();
         X509Certificate c2Cert = generateClientCertificate(keyPairClient2.getPublic(), "client2");
         System.out.println("Client 2 certificate: "+c2Cert.toString());
+        System.out.println();
+
+        RSA fakeCA = new RSA();
+        RSA keyPairClient3 = new RSA();
+        X509Certificate c3Cert = generateFakeCertificate(keyPairClient3.getPublic(), fakeCA.getPrivate() ,"client3");
+        System.out.println("Client 3 certificate: "+c3Cert.toString());
+        System.out.println();
+
+        System.out.println("Client 1 public key valid with CA: "+validateCertificate(c1Cert, keyPair.getPublic()));
+        System.out.println("Client 2 public key valid with CA: "+validateCertificate(c1Cert, keyPair.getPublic()));
+        System.out.println("Client 3 public key valid with CA: "+validateCertificate(c3Cert, keyPair.getPublic()));
     }
 
 }
